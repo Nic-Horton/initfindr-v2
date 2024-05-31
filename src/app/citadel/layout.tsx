f90c97367ae09ed2787aa08af82155498a16ca7f
Nicolas Horton <nic.horton00@gmail.com>
2024-05-30 21:07:44 -0500

    Added war room page ui

--- src/app/citadel/layout.tsx
+++ src/app/citadel/layout.tsx
@@ -9,7 +9,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="grid h-screen w-full pl-[53px]">
 			<Sidebar />
-
 			<div className="flex flex-col">
 				<header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
 					<h1 className="text-xl font-semibold">The Citadel</h1>
--- src/app/citadel/warroom/page.tsx
+++ src/app/citadel/warroom/page.tsx
@@ -1,5 +1,16 @@
+import BattleController from '@/components/BattleController';
+
 const WarRoom = () => {
-	return <div>war room</div>;
+	return (
+		<main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
+			<div className="relative hidden flex-col items-start gap-8 md:flex">
+				<BattleController />
+			</div>
+			<div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
+				Cards for players
+			</div>
+		</main>
+	);
 };
 
 export default WarRoom;
