97d307b6a521d2e47c7a43299b3fd77696913451
Nicolas Horton <nic.horton00@gmail.com>
2024-07-27 14:14:29 -0500

    Added spinners for loading when checking authentication

--- src/lib/RedirectIfAuth.tsx
+++ src/lib/RedirectIfAuth.tsx
@@ -2,6 +2,7 @@
 import { ReactNode, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import useAuthStore from '../store/authStore';
+import { Loader2 } from 'lucide-react';
 
 interface RedirectIfAuthenticatedProps {
 	children: ReactNode;
@@ -23,7 +24,11 @@ const RedirectIfAuthenticated = ({
 	}, [user, router]);
 
 	if (loading) {
-		return <div>Loading...</div>;
+		return (
+			<div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-stone-800 via-red-800 to-[#675f3b]">
+				<Loader2 className="mr-2 h-20 w-20 animate-spin" />
+			</div>
+		);
 	}
 
 	return <>{children}</>;
